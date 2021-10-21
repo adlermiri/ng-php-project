097cda8343a3a511954a6aa27444621b0319bf8b
adlermiri <m0556735705@gmail.com>
2021-10-21 17:24:27 +0300

    upload

---
+++ resources/views/angular.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Angular</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="/assets/angular/styles.09e2c710755c8867a460.css"></head>
+<body>
+  <app-root></app-root>
+<script src="/assets/angular/runtime.564f6e47d0f3d32ffe39.js" defer></script><script src="/assets/angular/polyfills.00096ed7d93ed26ee6df.js" defer></script><script src="/assets/angular/main.8e91774fce2e9a9f42f6.js" defer></script></body>
+</html>
